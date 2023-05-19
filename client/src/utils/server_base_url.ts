80e89cd4374c6adbbe4fcd9c6d01ff9df61eab15
p-alex <alexey9021@gmail.com>
2023-05-19 21:02:42 +0300

    Change server base url based on node env

--- client/src/utils/server_base_url.ts
+++ client/src/utils/server_base_url.ts
@@ -1,2 +1,3 @@
-export const SERVER_BASE_URL =
-  "https://wow-inspect-server.pistolalex.com/api/v1";
+export const SERVER_BASE_URL = import.meta.env.PROD
+  ? "https://wow-inspect-server.pistolalex.com/api/v1"
+  : "http://localhost:5000/api/v1";
